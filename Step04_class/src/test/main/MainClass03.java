b140898f00060ba1f50f42b283c0bbc363906c0c
shimyoungmin <dudals6965@gmail.com>
2020-11-19 17:44:40 +0900

    commit

--- Step04_class/src/test/main/MainClass03.java
+++ Step04_class/src/test/main/MainClass03.java
@@ -1,5 +1,7 @@
 package test.main;
 
+import java.io.PrintStream;
+
 import test.mypac.MyUtil;
 
 public class MainClass03 {
@@ -7,7 +9,8 @@ public class MainClass03 {
 	public static void main(String[] args) {
 		//static은 글꼴이 기울임체로 나타내어진다.
 		MyUtil.playMusic();
-
+		String name = MyUtil.owner;
+		System.out.println();
 	}
 
 }
